5e7ce1b6381b8a8fc429d77af1a91aea5bf9235d
JaferMin <jafer@aapi.io>
2019-06-24 13:05:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-editor-wysiwyg-f2sjjk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-editor-wysiwyg-f2sjjk)
---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular 6';
+  htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
+}
